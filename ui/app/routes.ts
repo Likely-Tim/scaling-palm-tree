993b07b393ca9ed6e8b0da37a91fda0041ce89e8
Timothy Wu <timothywu.mail@gmail.com>
2025-08-20 17:15:18 -0700

    Add redirect

--- ui/app/routes.ts
+++ ui/app/routes.ts
@@ -1,6 +1,6 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
+    index("routes/redirect.tsx"),
     route("overview", "routes/overview.tsx")
 ] satisfies RouteConfig;
---
+++ ui/app/routes/redirect.tsx
@@ -0,0 +1,10 @@
+import { redirect } from "react-router";
+import type { Route } from "../+types/root";
+
+export async function loader({ request }: Route.LoaderArgs) {
+    throw redirect("/overview");
+}
+
+export default function Redirect() {
+    return;
+}
