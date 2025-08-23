1bbcbd31f47e35f7e3342164c685ce7b8d96d657
Timothy Wu <timothywu.mail@gmail.com>
2025-08-23 10:40:05 -0700

    Add Nav Header

--- ui-next/app/layout.tsx
+++ ui-next/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Provider } from "./_components/ui/provider";
+import { NavHeader } from "./_components/nav_header";
+import { inter } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+        <Provider>
+          <NavHeader />
+          {children}
+        </Provider>
       </body>
     </html>
   );
