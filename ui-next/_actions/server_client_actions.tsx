c8d8359542300a936658d714288edb2994a0439b
Timothy Wu <timothywu.mail@gmail.com>
2025-08-24 18:41:48 -0700

    Add device filter

--- ui-next/_actions/server_client_actions.tsx
+++ ui-next/_actions/server_client_actions.tsx
@@ -5,3 +5,7 @@ import serverClient from "@/app/_clients/server_client";
 export async function registerDevice(entityId: string, friendlyName: string) {
   await serverClient.registerDevice(entityId, friendlyName);
 }
+
+export async function getStates() {
+  return await serverClient.getStates();
+}
--- ui-next/app/devices/page.tsx
+++ ui-next/app/devices/page.tsx
@@ -1,27 +1,10 @@
-import { Container, Flex, Text } from "@chakra-ui/react";
 import serverClient from "../_clients/server_client";
-import DeviceCard from "../_components/device_card";
 import { sortStates } from "../utils/state_utils";
+import DevicesPage from "./page_container";
 
 export default async function Page() {
   const states = await serverClient.getStates();
   sortStates(states);
 
-  return (
-    <Container>
-      <Flex flexWrap="wrap" gap="3" justifyContent="space-between">
-        {states.map((state) => {
-          return (
-            <DeviceCard
-              key={state.entity_id}
-              domain={state.domain}
-              friendlyName={state.attributes.friendly_name}
-              entityId={state.entity_id}
-              state={state.state}
-            />
-          );
-        })}
-      </Flex>
-    </Container>
-  );
+  return <DevicesPage states={states} />;
 }
