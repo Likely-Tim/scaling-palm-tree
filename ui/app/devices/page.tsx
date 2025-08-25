3d858fd2f51dcb6a13d7d7b98668d5c151a3d95e
Timothy Wu <timothywu.mail@gmail.com>
2025-08-24 19:23:49 -0700

    Modify metadata

--- ui/app/devices/page.tsx
+++ ui/app/devices/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from "next";
 import serverClient from "../_clients/server_client";
 import { sortStates } from "../utils/state_utils";
 import DevicesPage from "./page_container";
+
+export const metadata: Metadata = {
+  title: "Device Management",
+};
 
 export default async function Page() {
   const states = await serverClient.getStates();
