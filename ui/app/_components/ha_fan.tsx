1a259dc9673f6cc45c287debb160a3dd0fcf682f
Timothy Wu <timothywu.mail@gmail.com>
2025-08-26 18:46:36 -0700

    Add fan toggle

---
+++ ui/app/_components/ha_fan.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Switch } from '@chakra-ui/react';
+import { useState } from 'react';
+import { LiaSunSolid } from 'react-icons/lia';
+import { LuMoon } from 'react-icons/lu';
+import { modifyDeviceState } from '../_actions/server_client_actions';
+import { useRouter } from 'next/navigation';
+
+export interface HaFanProps {
+    state: string | undefined;
+    entityId: string;
+}
+
+export default function HaFan(props: HaFanProps) {
+    const [checked, setChecked] = useState(props.state === 'on' ? true : false);
+    const [disabled, setDisabled] = useState(false);
+    const router = useRouter();
+
+    return (
+        <Switch.Root
+            disabled={disabled}
+            size={'md'}
+            checked={checked}
+            onCheckedChange={e => {
+                modifyDeviceState('fan', 'toggle', props.entityId);
+                setChecked(e.checked);
+                setDisabled(true);
+                setTimeout(() => {
+                    setDisabled(false);
+                    router.refresh();
+                }, 1500);
+            }}
+        >
+            <Switch.HiddenInput />
+            <Switch.Label>Toggle</Switch.Label>
+            <Switch.Control>
+                <Switch.Thumb>
+                    <Switch.ThumbIndicator fallback={<LuMoon />}>
+                        <LiaSunSolid />
+                    </Switch.ThumbIndicator>
+                </Switch.Thumb>
+            </Switch.Control>
+        </Switch.Root>
+    );
+}
