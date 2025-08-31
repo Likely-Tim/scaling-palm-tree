2e51cd9b8682e5de26d40d983ec95dfb8dae8367
Ryan Tan <ryantan.mail@gmail.com>
2025-08-31 00:00:35 -0700

    api group member return full device data

--- server/palm/views/device_groups/members.py
+++ server/palm/views/device_groups/members.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
-from palm.models import DeviceGroupMembers, DeviceGroup
+from palm.models import DeviceGroupMembers, DeviceGroup, Device
 from palm.constants import env_constants
 
 @csrf_exempt
@@ -27,6 +27,8 @@ def index(request, group_id):
         logging.info("GET request for device groups")
         group = get_object_or_404(DeviceGroup, id=group_id, home_assistant_url=env_constants.HOME_ASSISTANT_URL)
         group_entities = DeviceGroupMembers.objects.filter(group=group)
-        groups_entities_data = list(group_entities.values('group_id', 'entity_id'))
-        result = {"group": model_to_dict(group), "entities": groups_entities_data}
+        group_entities_ids = [entity.entity_id for entity in group_entities]
+        logging.info("Device group entity ids found", group_entities_ids)
+        devices = Device.objects.filter(entity_id__in=group_entities_ids, home_assistant_url=env_constants.HOME_ASSISTANT_URL)
+        result = {"group": model_to_dict(group), "devices": [model_to_dict(device) for device in devices]}
         return JsonResponse(result)
